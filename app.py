25d8b2a9247fd36ba16764b47548bd9227952fe4
BinilTomJose1278 <biniltomjose12780@gmail.com>
2023-04-27 14:41:29 +0530

    Data Transfer from frontend to backend

--- app.py
+++ app.py
@@ -2,20 +2,26 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 
 app = Flask(__name__)
 app.secret_key = 'many random bytes'
-
-#app.config['MYSQL_HOST'] = 'localhost'
-#app.config['MYSQL_USER'] = 'root'
-#app.config['MYSQL_PASSWORD'] = ''
-#app.config['MYSQL_DB'] = 'trackmybus'
-#mysql = MySQL(app)
+usr=""
 
 @app.route('/')
 def home():
     return render_template('index1.html')
 
-@app.route('/Predict',methods=['POST'])
+
+@app.route('/Predict',methods=['GET','POST'])
 def pre():
     return render_template('index.html')
+
+@app.route('/Tpred',methods=['GET','POST'])
+def twe():
+     user=request.form["tweet"]    
+     return render_template('index.html',usr=user)
+
+ 
+
+          
+
 
 @app.route('/Fuzzy',methods=['POST'])
 def fuz():
